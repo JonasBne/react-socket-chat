a6e16dc27f2579a6d2a338b761a0945507f73340
Jonas <boone.jonas@outlook.be>
2022-03-23 15:51:30 +0100

    refactor: initial state is now based on contacts in local storage

--- client/src/pages/Dashboard/Contacts/ContactsSlice.ts
+++ client/src/pages/Dashboard/Contacts/ContactsSlice.ts
@@ -1,9 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Contact } from '../../../domain/Contact';
 
+const contactsInLocalStorage = () => {
+  const valueInStorage = window.localStorage.getItem('react-socketio-chat-contact');
+
+  if (valueInStorage) {
+    return JSON.parse(valueInStorage);
+  }
+  return [];
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [] as Contact[],
+  initialState: contactsInLocalStorage,
   reducers: {
     createContact(state, action: PayloadAction<Contact>) {
       state.push(action.payload);
