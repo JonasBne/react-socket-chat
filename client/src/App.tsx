8330c2963a38ce6bf469a2d0d6b06b02d26e2692
Jonas <boone.jonas@outlook.be>
2022-03-23 09:26:42 +0100

    feat: added login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello World</div>;
+  return <Login />;
 }
 
 export default App;
