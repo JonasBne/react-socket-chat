1eb72f1aadb0bad4a9d12dfc4baaa45b72b86b04
Jonas <boone.jonas@outlook.be>
2022-03-23 09:39:01 +0100

    refactor: added form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form, FormGroup, FormLabel, FormControl, FormText, Button } from 'react-bootstrap';
 
 export default function Login() {
-  return <Container>Login</Container>;
+  return (
+    <Container className="d-flex align-items-center" style={{ height: '100vh' }}>
+      <Form className="w-100">
+        <FormGroup controlId="login-form">
+          <FormLabel>Chatroom ID</FormLabel>
+          <FormControl type="text" required />
+          <FormText className="text-muted">Choose your own ID or generate a random number.</FormText>
+        </FormGroup>
+        <Button type="submit" className="m-1">
+          Login
+        </Button>
+        <Button type="button" variant="light" className="m-1">
+          Generate ID
+        </Button>
+      </Form>
+    </Container>
+  );
 }
