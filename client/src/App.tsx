e8e92a5613fbe9f38bb54a72be83a325b77ff562
Jonas <boone.jonas@outlook.be>
2022-03-23 10:06:25 +0100

    refactor: moved state to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import Login from './pages/Login';
+import React, { useState } from 'react';
+import Login from './pages/Login/Login';
 
 function App() {
-  return <Login />;
+  const [chatRoomId, setChatRoomId] = useState('');
+
+  const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setChatRoomId(evt.target.value);
+  };
+
+  console.log('chatroomid', chatRoomId);
+
+  return <Login chatRoomId={chatRoomId} onIdChange={handleInputChange} />;
 }
 
 export default App;
--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 import { Container, Form, FormGroup, FormLabel, FormControl, FormText, Button } from 'react-bootstrap';
 
-export default function Login() {
+interface LoginProps {
+  chatRoomId: string;
+  onIdChange: (evt: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Login({ chatRoomId, onIdChange }: LoginProps) {
+  const handleInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    onIdChange(evt);
+  };
+
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+  };
+
   return (
     <Container className="d-flex align-items-center" style={{ height: '100vh' }}>
-      <Form className="w-100">
+      <Form className="w-100" onSubmit={handleFormSubmit}>
         <FormGroup controlId="login-form">
           <FormLabel>Chatroom ID</FormLabel>
-          <FormControl type="text" required />
+          <FormControl type="text" required onChange={handleInputChange} value={chatRoomId} />
           <FormText className="text-muted">Choose your own ID or generate a random number.</FormText>
         </FormGroup>
         <Button type="submit" className="m-1">
